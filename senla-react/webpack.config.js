2a2db85909db72151752a1cc9b82d759d12dfb88
kaesvorelay <kaes_vorelay@icloud.com>
2021-11-09 18:02:06 +0300

    eighth iteration

---
+++ senla-react/webpack.config.js
@@ -0,0 +1,59 @@
+const path  = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { loader } = require('mini-css-extract-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+const filename = (ext) => isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`
+
+
+module.exports = {
+  entry: '../1/main.js',
+  mode: 'development', 
+  output: { 
+    filename: `./${filename('js')}` ,
+    path: path.resolve(__dirname, 'build')
+},
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node-modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/, 
+        use: ['style-loader','css-loader']
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        loader: 'file-loader',
+        options: {
+          name: 'cat.png'
+        }
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]'
+          }
+      }
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: '../1/index.html',
+      filename: 'index.html',
+      minify: {
+          collapseWhitespace: isProd,
+      }
+  }),
+    new CleanWebpackPlugin()
+  ]
+}
