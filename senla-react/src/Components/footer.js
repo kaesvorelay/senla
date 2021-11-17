02a68df584e47a5c3ca78785f2b0ce3c5f1efffe
kaesvorelay <kaes_vorelay@icloud.com>
2021-11-17 15:24:28 +0300

    ninth iteration

--- senla-react/src/Components/footer.js
+++ senla-react/src/Components/footer.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import kitty from '../kitty.png'
+import React from 'react';
+import kitty from '../kitty.png';
 
 class Footer extends React.Component {
   constructor(props) {
@@ -8,83 +8,104 @@ class Footer extends React.Component {
     // this.yellowBg = this.yellowBg.bind(this);
     // this.greenBg = this.greenBg.bind(this);
     // this.clearBg = this.clearBg.bind(this);
-    this.changeColor = this.changeColor.bind(this)
+    // this.changeColor = this.changeColor.bind(this)
+    this.prp = this.prp.bind(this);
+  }
+  prp(a) {
+    let changeColor = this.props.color;
+    console.log(changeColor(a));
   }
 
-  changeColor(col) {
-    if(col === "isRed") {
-      this.props.onRed({
-        red: false
-      })
-    } else if (col === "isYellow") {
-      this.props.onYellow({
-        yellow: false
-      })
-    } else if (col === "isGreen") {
-      this.props.onGreen({
-        greem: false
-      })
-    } else if (col === "clear") {
-      this.props.onClear({
-        clear: true
-      })
-    }
-  }
+  // changeColor(col) {
+  //   if(col === "isRed") {
+  //     this.props.onRed({
+  //       red: false
+  //     })
+  //   } else if (col === "isYellow") {
+  //     this.props.onYellow({
+  //       yellow: false
+  //     })
+  //   } else if (col === "isGreen") {
+  //     this.props.onGreen({
+  //       greem: false
+  //     })
+  //   } else if (col === "clear") {
+  //     this.props.onClear({
+  //       clear: true
+  //     })
+  //   }
+  // }
 
   render() {
     return (
       <footer className="footer">
-      <h3 className="footer-title">Footer title</h3>
-      <table className="table">
-        <tr>
-          <td>&nbsp;</td>
-          <th>M. Viedo</th>
-          <th>Eldorado</th>
-          <th>DNS</th>
-          <th>Our store</th>
-        </tr>
-        <tr>
-          <th>online order</th>
-          <td>+</td>
-          <td>-</td>
-          <td>+</td>
-          <td>+</td>
-        </tr>
-        <tr>
-          <th>Delivery</th>
-          <td>-</td>
-          <td>-</td>
-          <td>-</td>
-          <td>+ free</td>
-        </tr>
-        <tr>
-          <th>guarantee</th>
-          <td>+</td>
-          <td>+</td>
-          <td>+</td>
-          <td>+ 2 years</td>
-        </tr>
-        <tr>
-          <th>Discounts</th>
-          <td>-</td>
-          <td>-</td>
-          <td>+</td>
-          <td>+ 20% all clients</td>
-        </tr>
-      </table>
-      <a id="next-str" href="main.html">Next page</a>
-      <figure className="img-wrapper">
-        <img className="img" src={kitty} alt="cat" />
-        <figcaption className="img-title">
-          A little cat asks you to smile
-        </figcaption>
-      </figure>
-      <button onClick={()=>this.changeColor("isRed")} id="background-red">Red</button>
-      <button onClick={()=>this.changeColor("isYellow")} id="background-yellow">yellow</button>
-      <button onClick={()=>this.changeColor("isGreen")} id="background-green">green</button>
-      <button onClick={()=>this.changeColor("clear")} id="clear-background">clear</button>
-    </footer>
-    )
+        <h3 className="footer-title">Footer title</h3>
+        <table className="table">
+          <tr>
+            <td>&nbsp;</td>
+            <th>M. Viedo</th>
+            <th>Eldorado</th>
+            <th>DNS</th>
+            <th>Our store</th>
+          </tr>
+          <tr>
+            <th>online order</th>
+            <td>+</td>
+            <td>-</td>
+            <td>+</td>
+            <td>+</td>
+          </tr>
+          <tr>
+            <th>Delivery</th>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>+ free</td>
+          </tr>
+          <tr>
+            <th>guarantee</th>
+            <td>+</td>
+            <td>+</td>
+            <td>+</td>
+            <td>+ 2 years</td>
+          </tr>
+          <tr>
+            <th>Discounts</th>
+            <td>-</td>
+            <td>-</td>
+            <td>+</td>
+            <td>+ 20% all clients</td>
+          </tr>
+        </table>
+        <a id="next-str" href="main.html">
+          Next page
+        </a>
+        <figure className="img-wrapper">
+          <img className="img" src={kitty} alt="cat" />
+          <figcaption className="img-title">
+            A little cat asks you to smile
+          </figcaption>
+        </figure>
+        <button onClick={() => this.props.color('isRed')} id="background-red">
+          Red
+        </button>
+        <button
+          onClick={() => this.props.color('isYellow')}
+          id="background-yellow"
+        >
+          yellow
+        </button>
+        <button
+          onClick={() => this.props.color('isGreen')}
+          id="background-green"
+        >
+          green
+        </button>
+        <button onClick={() => this.props.color('clear')} id="clear-background">
+          clear
+        </button>
+      </footer>
+    );
   }
 }
 
--- senla-react/src/Pages/first-page.js
+++ senla-react/src/Pages/first-page.js
@@ -1,10 +1,10 @@
 import '../style.css';
-import React from 'react'
-import Header from '../Components/header'
-import Main from '../Components/main'
-import Aside from '../Components/aside'
-import Footer from '../Components/footer'
-import {Link} from 'react-router-dom'
+import React from 'react';
+import Header from '../Components/header';
+import Main from '../Components/main';
+import Aside from '../Components/aside';
+import Footer from '../Components/footer';
+import { Link } from 'react-router-dom';
 
 class All extends React.Component {
   constructor(props) {
@@ -13,58 +13,48 @@ class All extends React.Component {
       red: false,
       yellow: false,
       green: false
-    }
-    this.switchRed = this.switchRed.bind(this);
-    this.switchYellow = this.switchYellow.bind(this);
-    this.switchGreen = this.switchGreen.bind(this);
-    this.clear = this.clear.bind(this)
-  }
-  switchRed(){
-    this.setState({
-      red: true,
-      yellow: false,
-      green: false
-    })
-    console.log(this.state)
-  }
-  switchYellow(){
-    this.setState({
-      red: false,
-      yellow: true,
-      green: false
-    })
-    console.log(this.state)
-  }
-  switchGreen(){
-    this.setState({
-      red: false,
-      yellow: false,
-      green: true,
-    })
-    console.log(this.state)
+    };
+    this.changeCol = this.changeCol.bind(this);
   }
 
-  clear() {
-    this.setState({
-      red: false,
-      yellow: false,
-      green: false
-    })
-    localStorage.clear();
-    sessionStorage.clear();
+  changeCol(col) {
+    if (col === 'isRed') {
+      return this.setState({
+        red: true,
+        yellow: false,
+        green: false
+      });
+    } else if (col === 'isYellow') {
+      return this.setState({
+        red: false,
+        yellow: true,
+        green: false
+      });
+    } else if (col === 'isGreen') {
+      return this.setState({
+        red: false,
+        yellow: false,
+        green: true
+      });
+    } else if (col === 'clear') {
+      return this.setState({
+        red: false,
+        yellow: false,
+        green: false
+      });
+    }
   }
-  
-  
+
   render() {
     return (
       <div className="grid-container">
-        <Header color={this.state}/>
-        <Main/>
-        <Aside/>
-        <Footer onClear={this.clear} onRed={this.switchRed} onYellow={this.switchYellow} onGreen={this.switchGreen}/>
-        <Link to='hello'>Go</Link>
+        <Header color={this.state} />
+        <Main />
+        <Aside />
+        <Footer color={this.changeCol} />
+        <Link to="hello">Go</Link>
       </div>
-    )
+    );
   }
 }
 
